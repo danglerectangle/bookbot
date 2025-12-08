932c46b58a2f371bd5aa7b17a41a5f8221ef3136
danglerectangle <djt.rectangle@gmail.com>
2025-12-08 01:30:25 -0800

    added count_words function to main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def count_words():
+def count_words(book_text):
+    words_to_count = []
+    words_to_count = book_text.split()
+    print(f"Found {len(words_to_count)} total words")
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count_words(text)
 
 main()
