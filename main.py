27d1790ec097186e51b461c908151f4169efa038
danglerectangle <djt.rectangle@gmail.com>
2025-12-08 01:25:26 -0800

    added get_book_text function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words():
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
