18d2bc0aec7c131a704d9695c8fe839a458370bb
danglerectangle <djt.rectangle@gmail.com>
2025-12-09 14:48:35 -0800

    updated main.py and stats.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from stats import get_num_words
 from stats import get_num_char
+from stats import make_list_of_char_dict
+from stats import sort_on
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+def print_report(book_path, text):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    get_num_words(text)
+    print("--------- Character Count -------")
+    make_list_of_char_dict(text)
+    print("============= END ===============")
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    get_num_words(text)
-    get_num_char(text)
+    print_report(book_path, text)
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ words_to_count = [] # populated by get_num_words #
 chars_to_count = [] # populated by get_num_char #
 chars_count_dict = {} # populated by get_num_char #
 list_of_char_dict = [] # populated by make_list_of_char_dict #
-test_list_words = ["HELLO", "world"] # test list of strings for debugging #
+test_list_words = ["HELLO,", "world!"] # test list of strings for debugging #
 
 def get_num_words(book_text_string): # takes book text string, converts to list of strings, prints count of string length # 
     words_to_count = book_text_string.split()
@@ -12,8 +12,9 @@ def get_num_words(book_text_string): # takes book text string, converts to list 
 def get_num_char(book_text_list): # takes list of strings, returns a dictionary: {"a": 4, "b": 23, ...} #
     for word in book_text_list:
         for i in word:
-            lower_char = str.lower(i)
-            chars_to_count.append(lower_char)
+            if i.isalpha():
+                lower_char = str.lower(i)
+                chars_to_count.append(lower_char)
     
     for i in chars_to_count:
         chars_count_dict[i] = 0
@@ -23,7 +24,7 @@ def get_num_char(book_text_list): # takes list of strings, returns a dictionary:
     
     return chars_count_dict
 
-def make_list_of_char_dict(book_text_list): # takes a list of strings, makes a formatted list of dicts, then sorts them in descending order and returns it: [{"char": b, "num": 23}, {"char": a, "num": 4}, ...] # 
+def make_list_of_char_dict(book_text_list): # takes a list of strings, makes a formatted list of dicts, then sorts them in descending "num" order and returns it: [{"char": b, "num": 23}, {"char": a, "num": 4}, ...] # 
     get_num_char(book_text_list)
 
     for i in chars_count_dict:
@@ -32,9 +33,12 @@ def make_list_of_char_dict(book_text_list): # takes a list of strings, makes a f
         list_of_char_dict.append({"char": dict_key, "num": dict_value})
     
     list_of_char_dict.sort(reverse=True, key=sort_on)
-    return list_of_char_dict
 
-def sort_on(list_of_dict):
-    return list_of_dict["num"]
+    for i in list_of_char_dict:
+        print(f"{i["char"]}: {i["num"]}")
     
+def sort_on(list_of_dict): # sorting helper for make_list_of_char_dict #
+    return list_of_dict["num"]
+
+
 make_list_of_char_dict(test_list_words)
